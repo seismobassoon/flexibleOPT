c07d3917a8c4d38fab19e5e485f67886986201a2
Nobuaki Fuji <seismobassoon@gmail.com>
2025-04-19 15:03:45 +0200

    timemarching bon courage

--- OPTmotors/src/batchDiffTools.jl
+++ OPTmotors/src/batchDiffTools.jl
@@ -1,5 +1,40 @@
-using SparseDiffTools
+using SparseDiffTools,SparseArrays
 
-function myForwarddiff_color_jacobian!(J, Res_closed!, U, colorvec = colors)
-    return forwarddiff_color_jacobian!(J, Res_closed!, U, colorvec = colors)
+function sparseColouring()
+end
+function myForwarddiff_color_jacobian!(F, U,nEq)
+
+
+    # Sparsity pattern
+    input       = rand(ncx)
+    output      = similar(input)
+    Res_closed! = (F, U) -> Residual!(F, U, U0, U00, f, G, Gc, ρ, Δx, Δt, x, t)
+    sparsity    = Symbolics.jacobian_sparsity(Res_closed!, output, input)
+    J           = Float64.(sparse(sparsity))
+
+    # Makes coloring
+    colors      = matrix_colors(J)
+
+    # normalisation by the number of equations
+    normalisation = 1.0/nEq
+    r1 = 1.0
+    for iter in 1:10
+        Res_closed! = (F, U) -> Residual!(F, U)
+        r = norm(F)*normalisation
+        
+        if iter==1 r1 = r; end
+       
+        if r/r1 < 1e-8 break end
+            
+        # Jacobian assembly
+        forwarddiff_color_jacobian!(J, Res_closed!, U, colorvec = colors)
+
+        # Solve
+        δU   .= .-J\F
+
+        # update
+        U    .+= δU
+    end
+
+    return 
 end
--- OPTmotors/src/timeMarchingSchemes.jl
+++ OPTmotors/src/timeMarchingSchemes.jl
@@ -9,12 +9,6 @@ function timeMarchingScheme(opt, Nt, Δnum;sourceType="Ricker",t₀=50,f₀=0.03
     operators = opt["numOperators"]
     costfunctions,fieldLHS,fieldRHS,champsLimité = operators
     #@show size(costfunctions),size(fieldLHS[1,1]),size(fieldRHS)
-    @show champsLimité[1,1]
-
-    NField = size(costfunctions)[1]
-    NCostfunctions=size(costfunctions)[2]
-    if size(fieldLHS)[1]
-    end
 
     itVec=collect(1:1:Nt)
     t=(itVec.-1).*Δnum[end] # time vector # if it's not time marching t will give you just 0.0 regardless of Δnum[end]
@@ -39,10 +33,14 @@ function timeMarchingScheme(opt, Nt, Δnum;sourceType="Ricker",t₀=50,f₀=0.03
 
     #endregion
 
-    #region initial condition
+    #region initial condition, 1Dvectorisation of knowns and unknowns
 
-    tmpField = similar(fieldLHS)
-    tmpSource = similar(fieldRHS)
+    #@show champsLimité[1,1], fieldLHS[1:end,1:end-1][1:end]
+
+    knownField = fieldLHS[1:end,1:end-1][1:end]
+    unknownField = fieldLHS[1:end,end][1:end]
+    knownSource = similar(fieldRHS)
+    @show costfunctions[1:end]
 
     
 
@@ -62,51 +60,3 @@ function timeMarchingScheme(opt, Nt, Δnum;sourceType="Ricker",t₀=50,f₀=0.03
 
 
 end
-
-#=
-function dummy()
-# Time loop
-for it=1:nt
-    U00 .= U0
-    U0  .= U
-    t   += Δt 
-    noisy==true ? @printf("########### Step %06d ###########\n", it) : nothing
-
-    for i in eachindex(f)
-        sol   = Analytics(problem, @SVector([xc[i]; t-Δt])) # ???
-        fC    = sol.s 
-        sol   = Analytics(problem, @SVector([xc[i]-Δx; t-Δt]))
-        fW    = sol.s 
-        sol   = Analytics(problem, @SVector([xc[i]+Δx; t-Δt]))
-        fE    = sol.s 
-        sol   = Analytics(problem, @SVector([xc[i]; t-2*Δt]))
-        fS    = sol.s 
-        sol   = Analytics(problem, @SVector([xc[i]; t-0*Δt]))
-        fN    = sol.s 
-        f[i]  = fC - 1*( 4*fC - fW - fE - fS - fN )/12
-
-    end
-
-    r1 = 1.0
-    for iter=1:10
-
-        # Residual evaluation: T is found if F = 0
-        Residual!(F, U, U0, U00, f, G, Gc, ρ, Δx, Δt, x, t)
-        Res_closed! = (F, U) -> Residual!(F, U, U0, U00, f, G, Gc, ρ, Δx, Δt, x, t)
-        r = norm(F)/ncx
-        if iter==1 r1 = r; end
-        noisy==true ? @printf("## Iteration %06d: r/r1 = %1.2e ##\n", iter, r/r1) : nothing
-        if r/r1 < 1e-8 break end
-            
-        # Jacobian assembly
-        forwarddiff_color_jacobian!(J, Res_closed!, U, colorvec = colors)
-
-        # Solve
-        δU   .= .-J\F
-
-        # update
-        U    .+= δU
-    end
-end
-end
-=#
