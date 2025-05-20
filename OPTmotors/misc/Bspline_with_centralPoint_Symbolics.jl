1af87fdd0a0bfdbb8c3961fdb8dbe339ca43acef
Nobuaki Fuji <seismobassoon@gmail.com>
2025-05-20 18:09:20 +0200

    miaol

--- OPTmotors/misc/Bspline_with_centralPoint_Symbolics.jl
+++ OPTmotors/misc/Bspline_with_centralPoint_Symbolics.jl
@@ -4,7 +4,7 @@ include("../src/batchNewSymbolics.jl")
 
 # this is a home-made B-spline functions' plot in order to understand the validity of truncation at left and right extremeties
 
-@variables x
+@variables x Δx
 ∂x = Differential(x)
 
 # plot parameters
@@ -44,14 +44,15 @@ colors = [get(Makie.colorschemes[:Paired_5], (i - 1) / (numberNodes - 1)) for i 
 b = zeros(Num, numberNodes, numberNodes, maximumOrder)
 bX = zeros(Num,numberNodes, 2, maximumOrder)
 
+# b-splines derivatives
 
+b_deriv = zeros(Num,numberNodes, numberNodes,maximumOrder,maximumOrder)
 
 for ι in 0:1:maximumOrder-1
-    local fig = Figure()
-    local ax = Axis(fig[1, 1]; title="B-spline function of $ι-th order")
+    
     local neighbour = Int((1 - (-1)^ι) / 2)
     floor = Int((ι - neighbour) / 2)
-    @show ceiling = Int((ι + neighbour) / 2)
+    ceiling = Int((ι + neighbour) / 2)
     if ι === 0
         for ν in nodeIndices # this will run for all the ν related to nodes
             tmpν = ν - νₗ + 1
@@ -64,7 +65,7 @@ for ι in 0:1:maximumOrder-1
             tmpν = ν - νₗ + 1
             # the denominator for the ι>0
 
-            denominator = ι * Δy
+            denominator = ι * Δx
 
             # for the upgoing part
 
@@ -73,7 +74,7 @@ for ι in 0:1:maximumOrder-1
                 leftlimit = maximum((1, tmpν - ceiling))
                 for νSegment in leftlimit:1:rightlimit
                     tmpνSegment = νSegment #- νₗ + 1
-                    numerator = x - (ν - ceiling) * Δy
+                    numerator = x - (ν - ceiling) * Δx
                     b[tmpνSegment, tmpν, ι+1] += mySimplify(numerator / denominator * b[tmpνSegment, tmpν-neighbour, ι])
                 end
             end
@@ -85,11 +86,12 @@ for ι in 0:1:maximumOrder-1
                 leftlimit = maximum((1, tmpν - ceiling + 1))
                 for νSegment in leftlimit:1:rightlimit
                     tmpνSegment = νSegment #- νₗ + 1
-                    numerator = (ν + floor + 1) * Δy - x
+                    numerator = (ν + floor + 1) * Δx - x
                     b[tmpνSegment, tmpν, ι+1] += mySimplify(numerator / denominator * b[tmpνSegment, tmpν-neighbour+1, ι])
                 end
 
             end
+
         end
     end
 
@@ -119,27 +121,48 @@ for ι in 0:1:maximumOrder-1
 
     end
 
+    b[:,1,ι+1]=bX[:,1,ι+1]
+    b[:,end,ι+1]=bX[:,end,ι+1]
 
-    for ν in nodeIndices # this will run for all the ν related to nodes
-        tmpν = ν - νₗ + 1
-        for νSegment in nodeIndices
-            tmpνSegment = νSegment - νₗ + 1
-            local xs = range(nodes[tmpνSegment], nodes[tmpνSegment+1], length=pointPerSegment)
-            local ys = similar(xs)
-             # Precompute symbolic expression and turn into function
-            expr = b[tmpνSegment, tmpν, ι+1]
-            if ν === νₗ
-                expr = bX[tmpνSegment,1,ι+1]
-            elseif ν === νᵣ
-                expr = bX[tmpνSegment,2,ι+1]
-            end
-            f_expr = Symbolics.build_function(expr, x; expression = false)
-            f = eval(f_expr)  # Now this works
-            ys =f.(xs)
-            lines!(ax, xs, ys, color=color = colors[tmpν])
+
+
+    # computing the derivatives 
+
+    for i in 0:1:maximumOrder-1 
+        if i === 0
+            b_deriv[:,:,i+1,ι+1] = b[:,:,ι+1]
+        else
+            b_deriv[:,:,i+1,ι+1] = mySimplify.(∂x.(b_deriv[:,:,i,ι+1]))
+        
         end
     end
-    #axislegend(ax)
 
-    display(fig)
+    for i in 0:1:maximumOrder-1 
+        local fig = Figure()
+        local ax = Axis(fig[1, 1]; title="B-spline function of $ι-th order, $i-th derivative")
+        for ν in nodeIndices # this will run for all the ν related to nodes
+            tmpν = ν - νₗ + 1
+            for νSegment in nodeIndices
+                tmpνSegment = νSegment - νₗ + 1
+                local xs = range(nodes[tmpνSegment], nodes[tmpνSegment+1], length=pointPerSegment)
+                local ys = similar(xs)
+                # Precompute symbolic expression and turn into function
+                expr = substitute(b_deriv[tmpνSegment, tmpν, i+1,ι+1],Dict(Δy=>Δx))
+                #if ν === νₗ
+                #    expr = bX[tmpνSegment,1,ι+1]
+                #elseif ν === νᵣ
+                #    expr = bX[tmpνSegment,2,ι+1]
+                #end
+                f_expr = Symbolics.build_function(expr, x; expression = false)
+                f = eval(f_expr)  # Now this works
+                ys =f.(xs)
+                lines!(ax, xs, ys, color=color = colors[tmpν])
+            end
+        end
+        #axislegend(ax)
+
+        display(fig)
+
+    end
+
 end
