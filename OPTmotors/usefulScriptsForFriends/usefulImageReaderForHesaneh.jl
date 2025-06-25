7a39e20e464c0f2f699b90628de4d16a601769d8
Nobuaki Fuji <seismobassoon@gmail.com>
2025-06-25 17:22:05 +0200

    path pb

--- OPTmotors/usefulScriptsForFriends/usefulImageReaderForHesaneh.jl
+++ OPTmotors/usefulScriptsForFriends/usefulImageReaderForHesaneh.jl
@@ -9,8 +9,8 @@ include("../src/imageReader.jl")
 
 #imagefile="DSM1D/data/model/artemis/IMG_6098.jpeg"
 #imagefile="DSM1D/data/model/random/tmp.png"
-imagefile = "DSM1D/data/model/moi/ground_canyon.png"
-colormap = "jet" #colormap can be RGB vector or predefined colormap
+imagefile = "../data/model/moi/ground_canyon.png"
+colormap = "hot" #colormap can be RGB vector or predefined colormap
 
 floatMatrix=read2DimageModel(imagefile,colormap;min=1.0,max=3.3, showRecoveredImage=true) 
 
