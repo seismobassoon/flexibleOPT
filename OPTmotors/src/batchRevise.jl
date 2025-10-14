8b49ce2d1aad156461952b692d02bc9bdbf58524
Nobuaki Fuji <seismobassoon@gmail.com>
2025-10-14 10:54:50 +0200

    myInclude works

--- OPTmotors/src/batchRevise.jl
+++ OPTmotors/src/batchRevise.jl
@@ -3,33 +3,29 @@
 
 Include or track a file depending on whether `Revise` is active.
 
-- If Revise is loaded, calls `Revise.includet(file)`
-- Otherwise, calls plain `myInclude(file)`
+Revise will be automatically loaded (if the package exists in the environment)
 
 This lets you develop interactively with Revise and
 run in production mode without modification.
 """
-#module BatchRevise
 
-    export myInclude, using_revise
 
-    # Try to activate Revise automatically
-    const using_revise = try
-        @eval using Revise
-        true
-    catch
-        @warn "Revise not found; falling back to myInclude."
-        false
+# Try to activate Revise automatically
+using_revise = try
+    @eval using Revise
+    true
+catch
+    @warn "Revise not found; falling back to myInclude."
+    false
+end
+
+"Smart myInclude: uses Revise.includet if Revise is active, otherwise include()."
+function myInclude(file::AbstractString)
+    if using_revise #&& hasproperty(Main, :Revise)
+        @info "Including with Revise: $file"
+        @eval Main Revise.includet($file)
+    else
+        include(file)
     end
+end
 
-    "Smart myInclude: uses Revise.includet if Revise is active, otherwise myInclude()."
-    function myInclude(file::AbstractString)
-        if using_revise && hasproperty(Main, :Revise)
-            @info "Including with Revise: $file"
-            @eval Main Revise.includet($file)
-        else
-            include(file)
-        end
-    end
-
-#end # module
