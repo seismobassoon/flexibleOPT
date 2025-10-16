0de8d508a6c42c397162feee4276c10f4f7a3658
Nobuaki Fuji <seismobassoon@gmail.com>
2025-10-16 17:12:05 +0200

    need to finalise the thing

--- OPTmotors/src/DSM1D.jl
+++ OPTmotors/src/DSM1D.jl
@@ -16,26 +16,25 @@ module DSM1D
 
 
 
-
-#greet() = print("Hello World!")
-
-
-# preprocessing 
-myInclude("mainStructures.jl")
-export compute1DseismicParamtersFromPolynomialCoefficients
-myInclude("inputParams.jl")
-myInclude("inputModels.jl")
-
-# if geodynamic model -> use another function and get parameter conversion
-modelFile=dirname(@__FILE__)*"/"*DSM1D.dsm1Dconfig.modelFolder*"/"*DSM1D.input.modelFile
-@time my1DDSMmodel=read1DModel(modelFile)
-
-# for kernel computations, it is still interesting to make a regular grid in r and θ
+    #greet() = print("Hello World!")
 
 
+    # preprocessing 
+    include("mainStructures.jl")
+    export compute1DseismicParamtersFromPolynomialCoefficients
+    include("inputParams.jl")
+    include("inputModels.jl")
 
-# Here we make vertical grids in radius for the DSM computation
-myInclude("DSM1Dmotor.jl")
+    # if geodynamic model -> use another function and get parameter conversion
+    modelFile=dirname(@__FILE__)*"/"*DSM1D.dsm1Dconfig.modelFolder*"/"*DSM1D.input.modelFile
+    @time my1DDSMmodel=read1DModel(modelFile)
+
+    # for kernel computations, it is still interesting to make a regular grid in r and θ
+
+
+
+    # Here we make vertical grids in radius for the DSM computation
+    include("DSM1Dmotor.jl")
 
 
 end # module DSM1D
